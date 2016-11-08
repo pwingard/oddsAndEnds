0e91a0449d7b3c2226078f2d78c16fa0e6a4064d
Pete Wingard <petewingard@gmail.com>
2016-11-08 16:32:13 -0500

    initial files

---
+++ fizzbuzz.php
@@ -0,0 +1,35 @@
+<?php
+//FizzBuzz
+//The programer's test 
+//From stackoverflow's LeonardChallis
+foreach(range (1,100) as $i) echo(''==($x=($i%3==0?"FIZZ":"").($i%5==0?"BUZZ":""))?$i:$x).'<br />';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
